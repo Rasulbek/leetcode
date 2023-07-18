b931ee0b6e1d383d7fc6cf814f9f229474a6e5ca
rasulbek <mr.abdurasuloff@gmail.com>
2023-07-18 21:19:44 +0500

    ZigZagConversion

--- src/leetcode/ZigZagConversion.kt
+++ src/leetcode/ZigZagConversion.kt
@@ -2,33 +2,70 @@ package leetcode
 
 
 fun main() {
-    println(ZigZagConversion().convert("PAYPALISHIRING", 3))
-    println(ZigZagConversion().convert("PAYPALISHIRING", 4))
-    println(ZigZagConversion().convert("A", 1))
+//    println(ZigZagConversion().convert("PAYPALISHIRING", 3))
+//    println(ZigZagConversion().convert("PAYPALISHIRING", 4))
+//    println(ZigZagConversion().convert("A", 1))
+    println(ZigZagConversion().strStr("asaadbutsad", "sad"))
 }
 
 class ZigZagConversion {
 
+    fun strStr(haystack: String, needle: String): Int {
+        var i = 0
+        val l = needle.length
+        if (l > haystack.length) {
+            return -1
+        }
+        while (i <= haystack.length - l) {
+            if (haystack.substring(i, i+l) == needle) {
+                return i
+            }
+            i++
+        }
+        return -1
+    }
+
     fun convert(s: String, numRows: Int): String {
         val array = s.toCharArray()
-        val result: String
+        var result: String = ""
         var matrix = Array(numRows) {
-            Array((s.length / 3) + 1) { ' ' }
+            Array(s.length) { ' ' }
         }
 
         matrix[0][0] = array[0]
 
         var i = 0
         var j = 0
-        while (i < result.size) {
-            result[i] = array[j]
-            j += (numRows - 1) * 2
-            j %= result.size
-//            j--
-            i++
+        var c = 0
+        var pastga = true
+        var tepaga = false
+        while (c < array.size) {
+            matrix[i][j] = array[c]
+            if (pastga) {
+                i++
+            }
+            if (tepaga) {
+                i--
+                j++
+            }
+            if (i == numRows - 1) {
+                tepaga = true
+                pastga = false
+            }
+            if (i == 0) {
+                tepaga = false
+                pastga = true
+            }
+            c++
         }
 
-        return result.joinToString("")
+        for (i in 0 until numRows) {
+            for(j in s.indices) {
+                if (matrix[i][j] != ' ') result += matrix[i][j]
+            }
+        }
+
+        return result.replace(" ", "")
     }
 
 }
